264fd83bbfbbad67a4bc0ffa54324b9228f1b035
titaniumlizard445 <parker.swenson@ucas-edu.net>
2026-01-21 09:53:17 -0700

    Created attribute management and inspect character

---
+++ Parker's Projects/inspect_character.py
@@ -0,0 +1,8 @@
+#PS 1st Character inspection
+
+#function for character inspection
+    #Display list of characters
+    #Input for which character they want to inspect
+    #Display options for what part of the character they would like to inspect (Class, Level, Inventory, Attributes, skills, Leave ,or Help Button)
+    #Input for choosing one of the displayed options
+    #
