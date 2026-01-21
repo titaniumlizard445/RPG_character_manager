30ecf74e92c98f578d2ae688e93da32859c8682f
Stubbed-My-Toe <isaac.covington@ucas-edu.net>
2026-01-21 09:53:10 -0700

    defining skills

---
+++ Skills (THIS IS ISAAC'S THING)/skills.py
@@ -0,0 +1,8 @@
+#Isaac Covington CP2 Skill Management
+
+#wizard skills = {Magic Missile, Fireball}
+#fighter skills = {Extra strike, Heavy Strike}
+#bard skills = {Inspire, Theme Song}
+#rogue skills = {Sneak, Assassinate}
+
+#Attribute
