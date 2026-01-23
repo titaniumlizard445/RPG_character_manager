ecbbc09c2671750cc17da23ad677a20053b10dcb
titaniumlizard445 <parker.swenson@ucas-edu.net>
2026-01-22 19:03:08 -0700

    Update attribute_management.py

--- Parker's Projects/attribute_management.py
+++ Parker's Projects/attribute_management.py
@@ -1,1 +1,15 @@
 #PS 1st attribute manager
+
+#function called attribute_manager(character dictionary, character chosen)
+  #Display attributes of chosen character
+  #Input for user to choose if they want to change any of them
+  #if yes
+    #for each attribute
+      #display the attribute
+      #ask user if they want to change that attribute
+      #if yes
+        #ask for what they want to update the value to
+        #Update the value
+      #if no
+        #go to next attribute (aka continue)
+    #display that it is done with the attribute changer
