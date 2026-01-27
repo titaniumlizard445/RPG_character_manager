d41a04036587696a1466d77a7435a1cfc8412c41
titaniumlizard445 <parker.swenson@ucas-edu.net>
2026-01-27 09:53:57 -0700

    Mostly coded inventory changer and finished attribute manager

--- Parker's Projects/attribute_management.py
+++ Parker's Projects/attribute_management.py
@@ -1,16 +1,26 @@
 #PS 1st attribute manager
 
 #function called attribute_manager(character dictionary, character chosen)
+def attribute_manager(char_dict,character):
   #Display attributes of chosen character
+  print(char_dict[character]["stats"])
   #Input for user to choose if they want to change any of them
+  use = input("Would you like to change any of the attributes? (y/n): ").strip().lower()
   #if yes
+  if use == "y":
     #for each attribute
+    for x in char_dict[character]["stats"]:
       #display the attribute
+      print(f"Would you like to change: {char_dict[character]["stats"][x]}")
       #ask user if they want to change that attribute
+      change = input("Enter here y/n: ").strip().lower()
       #if yes
+      if change == "y":
         #ask for what they want to update the value to
+        new_value = int(input(f"Enter the number for: {char_dict[character]["stats"][x.keys()]} here:").strip())
         #Update the value
-      #if no
-        #go to next attribute (aka continue)
+        char_dict[character]["stats"][x] = new_value
     #display that it is done with the attribute changer
+    print("attribute changer is done")
   #return character dictionary
+  return char_dict
--- Parker's Projects/inspect_character.py
+++ Parker's Projects/inspect_character.py
@@ -1,42 +1,89 @@
 #PS 1st Character inspection
 
+#import from pryors stuff
+
 #function for inventory changer (character chosen, character dictionary)
+def inventory_changer(character,char_dict):
     #loop until user is done with inventory management
+    while True:
         #greet user
+        print("Welcome to the inventory changer\n")
         #display options for inventory (change inventory, display inventory or leave)
+        print("\nWould you like to \n1)change inventory \n2)display inventory\n3)leave\n")
         #loop until user chooses valid option
+        while True:
             #input to choose option
+            choice = input("Enter the number here: ").strip()
             #if the valid option is chosen
-                #leave loop
-            #else
-                #display to user how to fix their input
+            #leave loop
+            match choice:
+                case "1":
+                    break
+                case "2":
+                    break
+                case "3":
+                    break
+                #else
+                    #display to user how to fix their input
+                case _:
+                    print("please input a valid value")
         #if display inventory is chosen
+        if choice == "1":
             #display the inventory
+            print(f"{character}'s inventory consists of: {char_dict[character]["inventory"]}")
         #if change inventory is chosen
+        if choice == "2":
             #loop until done with inventory changer
+            while True:
                 #Ask to add or remove items from the inventory
+                type_of_change = input("Would you like to add items or remove items from your inventory?\nEnter here (add/remove):").strip().lower()
                 #if add is chosen
+                if type_of_change == "add":
                     #create storage dictionary for all values
+                    storage = {}
                     #have user input name of the item
+                    name = input("What is the name of the item: ").strip().title()
                     #have user input the value
+                    worth = float(input(f"What is number for the worth of {name}").strip())
                     #have user input a description
-                    #have user input attributes
+                    desc = input(f"Please write a description for {name} here:").strip()
+                    #have user input an attribute
+                    attribute = input("Please write a attribute in the form of: (attribute:what the attribute does) here:").strip()
                     #store all the of the information in the dictionary
+                    storage = {
+                        "name":name,
+                        "worth":worth,
+                        "description":desc,
+                        "attribute":attribute}
                     #add that to the player's inventory
+                    char_dict[character]["inventory"].update(storage)
                 #if remove was chosen
+                if choice == "3":
                     #ask for the name of the item to remove or put leave if they don't want to input anything
-                    #if the input is leave
-                        #tell user they left remover
+                    if not char_dict[character]["inventory"]:
+                        #if the input is leave
+                            #tell user they left remover
+                        print("You don't have anything in the inventory to remove, leaving item remover...")
                     #else
+                    else:
                         #find the item in the inventory
+                        item_to_remove = input("Please Enter the name of the item you would like to remove here: ").strip().title()
                         #confirm that they want to remove this item
+                        certain = input("Are you sure you want to remove this item? (y/n):").strip().lower()
                         #if yes
+                        if certain == "y":
                             #remove it
+                            char_dict[character]["inventory"].remove(item_to_remove)
                 #ask if the user is done with the inventory changer
+                done = input("are you done using the item changer? (y/n): ")
                 #if yes
+                if done == "y":
                     #break out of loop
+                    break
         #ask if user is done with the inventory changer
+        if done == "y":
             #if yes
+            break
                 #leave
     #return character dictionary
 
